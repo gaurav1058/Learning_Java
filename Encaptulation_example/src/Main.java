9138d875fca53e86e9bc4c638e1c810fed61a0b9
GAURAV KATIYAR <gauravkatiyar2002@gmail.com>
2021-08-10 23:47:46 +0530

    Added encaptulation with example code

---
+++ Encaptulation_example/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // incaptulation example with code
+        // Printer printer =new Printer(50, true);
+        // int pagesPrinted=printer.printPages(4);
+        // System.out.println(pagesPrinted + " "+ printer.getPagesPrinted());
+        // pagesPrinted = printer.printPages(2);
+        // System.out.println(pagesPrinted + " "+ printer.getPagesPrinted());
+    }
+}
