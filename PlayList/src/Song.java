0a3ecd15fa179a7f9cb7408c1201eac7a6212baf
GAURAV KATIYAR <gauravkatiyar2002@gmail.com>
2021-08-26 17:15:39 +0530

    Added a application of song playlist using linked list

---
+++ PlayList/src/Song.java
@@ -0,0 +1,18 @@
+public class Song {
+    private String name;
+    private double duration;
+
+    public Song(String name, double duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public String toString(){
+        return this.name+ " : "+ this.duration;
+    }
+}
